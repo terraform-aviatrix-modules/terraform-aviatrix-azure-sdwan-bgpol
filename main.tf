35bb09ea911117f9920b1010d1a39a0a0cb2c9df
Dennizz <root@ipaddr.nl>
2021-03-06 10:59:00 +0100

    updates

--- main.tf
+++ main.tf
@@ -9,18 +9,27 @@ resource "aviatrix_vpc" "default" {
   aviatrix_transit_vpc = false
 }
 
-resource "azurerm_virtual_network_peering" "sdwan_to_transit" {
-  name                      = "${var.name}-to-transit"
-  resource_group_name       = var.transit_vnet.resource_group
-  virtual_network_name      = aviatrix_vpc.default.azure_vnet_resource_id
-  remote_virtual_network_id = var.transit_vnet.azure_vnet_resource_id
-}
+# resource "azurerm_virtual_network_peering" "sdwan_to_transit" {
+#   name                      = "${var.name}-to-transit"
+#   resource_group_name       = var.transit_vnet.resource_group
+#   virtual_network_name      = aviatrix_vpc.default.azure_vnet_resource_id
+#   remote_virtual_network_id = var.transit_vnet.azure_vnet_resource_id
+# }
 
-resource "azurerm_virtual_network_peering" "transit_to_sdwan" {
-  name                      = "transit_to_${var.name}"
-  resource_group_name       = var.transit_vnet.resource_group
-  virtual_network_name      = var.transit_vnet.azure_vnet_resource_id
-  remote_virtual_network_id = aviatrix_vpc.default.azure_vnet_resource_id
+# resource "azurerm_virtual_network_peering" "transit_to_sdwan" {
+#   name                      = "transit_to_${var.name}"
+#   resource_group_name       = var.transit_vnet.resource_group
+#   virtual_network_name      = var.transit_vnet.azure_vnet_resource_id
+#   remote_virtual_network_id = aviatrix_vpc.default.azure_vnet_resource_id
+# }
+
+resource "aviatrix_azure_peer" "sdwan_transit_peering" {
+  account_name1             = var.account
+  account_name2             = var.account
+  vnet_name_resource_group1 = aviatrix_vpc.default.vpc_id
+  vnet_name_resource_group2 = var.transit_gw.vpc_id
+  vnet_reg1                 = var.region
+  vnet_reg2                 = var.transit_gw.vpc_reg
 }
 
 resource "aviatrix_transit_external_device_conn" "ex-conn" {

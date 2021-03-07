80eba58851315622ac7ae044d2a05b7b9dc13afc
Dennizz <root@ipaddr.nl>
2021-03-07 20:55:08 +0100

    output

--- output.tf
+++ output.tf
@@ -1,3 +1,7 @@
 output "sdwan_pub_ip" {
   value = azurerm_public_ip.public_ip.ip_address
 }
+
+output "connection_name" {
+  value = aviatrix_transit_external_device_conn.default.connection_name
+}
